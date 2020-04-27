f5f4698681108ad1c69e64e8d9d1c927f891cf8f
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-04-27 10:57:11 +0200

    finished postGameModel() in dataService
    builds solus linux, jdk 11

--- Hangman-ConsoleApp/Services/src/tech/beryllium/services/DataService.java
+++ Hangman-ConsoleApp/Services/src/tech/beryllium/services/DataService.java
@@ -18,8 +18,12 @@ public class DataService {
         this.id = id;
         this.getUrl = new URL(String.format("https://beryllium.tech/api/HangmanGameModels/%x", id));
 
+        this.postUrl = new URL("https://beryllium.tech/api/HangmanGameModels");
+
         this.getHttpURLConnection = (HttpURLConnection) this.getUrl.openConnection();
         this.getHttpURLConnection.setRequestMethod("GET");
+
+        this.gson = new Gson();
     }
 
     public DataService() {
@@ -29,7 +33,9 @@ public class DataService {
 
     private int id;
     private URL getUrl;
+    private URL postUrl;
     private HttpURLConnection getHttpURLConnection;
+    private Gson gson;
 
     public GameDataModel getGameDataModel() throws IOException {
 
@@ -68,9 +74,80 @@ public class DataService {
     }
 
     private GameDataModel ToGameDataModel(String json) {
-        GameDataModel output = new Gson().fromJson(json, GameDataModel.class);
+        GameDataModel output = this.gson.fromJson(json, GameDataModel.class);
         return output;
     }
 
+    private String ToJson(GameModel gameModel) {
+        String rawJson = this.gson.toJson(gameModel);
+        return rawJson;
+    }
+
+    private String postJson(String json) throws IOException {
+        var connection = (HttpURLConnection) this.postUrl.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setDoOutput(true);
+
+        var jsonBytes = json.getBytes("utf-8");
+        connection.getOutputStream().write(jsonBytes, 0, jsonBytes.length);
+
+        var buffredReader = new BufferedReader(
+                new InputStreamReader(
+                        connection.getInputStream(),
+                        "utf-8"));
+        String line;
+        var response = new StringBuilder();
+        while ((line = buffredReader.readLine()) != null) {
+            response.append(line);
+        }
+
+        return response.toString();
+
+    }
+
+    private String putJson(String json) throws IOException {
+        var connection = (HttpURLConnection) this.postUrl.openConnection();
+        connection.setRequestMethod("PUT");
+        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setDoOutput(true);
+
+        var jsonBytes = json.getBytes("utf-8");
+        connection.getOutputStream().write(jsonBytes, 0, jsonBytes.length);
+
+        var buffredReader = new BufferedReader(
+                new InputStreamReader(
+                        connection.getInputStream(),
+                        "utf-8"));
+        String line;
+        var response = new StringBuilder();
+        while ((line = buffredReader.readLine()) != null) {
+            response.append(line);
+        }
+
+        return response.toString();
+    }
+
+    private GameDataModel postGameModel(GameModel gameModel) throws IOException {
+        var json = ToJson(gameModel);
+        //to json
+        String response;
+        if (id == 0) {
+            response = postJson(json);
+        } else {
+            response = putJson(json);
+        }
+        //post
+        GameDataModel returnModel = null;
+        if (response != null || response != "") {
+            returnModel = ToGameDataModel(response);
+        }
+        //from json
+        return  returnModel;
+        //return
+
+    }
 
 }
